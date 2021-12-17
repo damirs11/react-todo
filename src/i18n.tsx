2fbba5852c934858e0b2453ae81170bcda5dd639
DSalikhov <DSalikhov@blogic.ru>
2021-12-18 02:18:38 +0500

    delete luxon

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -2,7 +2,6 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
-import { DateTime } from 'luxon';
 
 i18n
   .use(Backend)
